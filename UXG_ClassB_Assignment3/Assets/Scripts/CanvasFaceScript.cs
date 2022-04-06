c1c3db7472b51b8675a97b2b8cbffeb1a2bcf722
trxaH <triciahengsiqi@gmail.com>
2022-04-06 21:08:54 +0800

    fsfdsf

--- UXG_ClassB_Assignment3/Assets/Scripts/CanvasFaceScript.cs
+++ UXG_ClassB_Assignment3/Assets/Scripts/CanvasFaceScript.cs
@@ -1,18 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
 
 public class CanvasFaceScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string charaRefID;
+
+    public Image faceShape;
+    public Image leftEye;
+    public Image rightEye;
+    public Image nose;
+    public Image mouth;
+
+    //to call individual parts of character
+    private static AsyncOperationHandle<Sprite> loadFace;
+    private static AsyncOperationHandle<Sprite> loadLefteye;
+    private static AsyncOperationHandle<Sprite> loadRighteye;
+    private static AsyncOperationHandle<Sprite> loadNose;
+    private static AsyncOperationHandle<Sprite> loadMouth;
+
+    public void SetCharaFace(Character cHara)
     {
-        
+
+        //get character face shape from Character script
+        PartShape charaShape = cHara.GetShape();
+        // Common.DebugLog(cHara.GetName() + "charaShape " + charaShape.GetDisplayName()); //need to add GetDisplayName();
+        if (charaShape != null)
+        {
+            //load face sprite
+            loadFace = Addressables.LoadAssetAsync<Sprite>(string.Format("Assets/Images/shape_{0}.png", charaShape.GetAssetName()));
+            loadFace.Completed += (sp) =>
+            {
+                //result of LoadAssetAsync
+                faceShape.sprite = sp.Result;
+                faceShape.color = cHara.GetColour();
+            };
+        }
+
+        //get character eye shape from Character script
+        PartEyes charaEyes = cHara.GetEyes();
+        // Common.DebugLog(cHara.GetName() + "charaEyes " + charaEyes.GetDisplayName()); //need to add GetDisplayName();
+        if (charaEyes != null)
+        {
+            //load left eye sprite
+            loadLefteye = Addressables.LoadAssetAsync<Sprite>(string.Format("Assets/Images/lefteye_{0}.png", charaEyes.GetAssetName()));
+            loadLefteye.Completed += (sp) =>
+            {
+                //result of LoadAssetAsync
+                leftEye.sprite = sp.Result;
+            };
+
+            //load right eye sprite
+            loadRighteye = Addressables.LoadAssetAsync<Sprite>(string.Format("Assets/Images/righteye_{0}.png", charaEyes.GetAssetName()));
+            loadRighteye.Completed += (sp) =>
+            {
+                //result of LoadAssetAsync
+                rightEye.sprite = sp.Result;
+            };
+        }
+
+        //get character nose shape from Character script
+        PartNose charaNose = cHara.GetNose();
+        //Common.DebugLog(cHara.GetName() + "charaNose " + charaNose.GetDisplayName()); //need to add GetDisplayName();
+        if (charaNose != null)
+        {
+            //load nose sprite
+            loadNose = Addressables.LoadAssetAsync<Sprite>(string.Format("Assets/Images/nose_{0}.png", charaNose.GetAssetName()));
+            loadNose.Completed += (sp) =>
+            {
+                //result of LoadAssetAsync
+                nose.sprite = sp.Result;
+            };
+
+
+            //get character mouth shape from Character script
+            PartMouth charaMouth = cHara.GetMouth();
+            // Common.DebugLog(cHara.GetName() + "charaMouth " + charaMouth.GetDisplayName()); //need to add GetDisplayName();
+            if (charaMouth != null)
+            {
+                //load nose sprite
+                loadMouth = Addressables.LoadAssetAsync<Sprite>(string.Format("Assets/Images/mouth_{0}.png", charaMouth.GetAssetName()));
+                loadMouth.Completed += (sp) =>
+                {
+                    //result of LoadAssetAsync
+                    mouth.sprite = sp.Result;
+                };
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float progress = 0;
+        if (loadFace.IsValid())
+        {
+            progress = (loadFace.PercentComplete + loadLefteye.PercentComplete + loadRighteye.PercentComplete + loadNose.PercentComplete + loadMouth.PercentComplete) / 4f;
+        }
+        if (progress > 0 && progress < 1f)
+        {
+            Common.DebugLog("face" + progress);
+        }
     }
 }
---
+++ UXG_ClassB_Assignment3/Assets/Scripts/Faces.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Faces : MonoBehaviour
+{
+    public List<GameObject> facelist;
+
+    public void UpdateFaces()
+    {
+        List<Character> charaList = Game.GetCharacterList();
+        Player player = Game.GetPlayer();
+
+        for (int i = 0; i < facelist.Count; i++)
+        {
+            UpdateEveryFace(facelist[i], charaList[i], player.GetPlayerCharacter() == charaList[i].GetRefId());
+        }
+    }
+
+    private void UpdateEveryFace(GameObject face, Character chara, bool isPlayer)
+    {
+        //character name
+        face.GetComponentInChildren<Text>().text = isPlayer ? "You" : chara.GetName();
+
+        face.GetComponentInChildren<CanvasFaceScript>().SetCharaFace(chara);
+    }
+}
