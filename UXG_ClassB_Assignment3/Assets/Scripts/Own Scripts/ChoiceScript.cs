98093b6e3d0a41dde889086db74e4aefe1a98a42
zoezoeokzoe <xzhuoxi@gmail.com>
2022-04-09 22:28:50 +0800

    helpp idk why cannot make selection

--- UXG_ClassB_Assignment3/Assets/Scripts/Own Scripts/ChoiceScript.cs
+++ UXG_ClassB_Assignment3/Assets/Scripts/Own Scripts/ChoiceScript.cs
@@ -10,11 +10,35 @@ public class ChoiceScript : MonoBehaviour
   public GameObject ChoiceSeki;
   public GameObject ChoiceAzure;
 
+  public int ChoiceMade;
+
+  public void QingChoiceSelection(){
+      TextBox.GetComponent<Text>().text = "Okay do you want my nose, face, left eye, right eye or mouth?";
+      ChoiceMade=1;
+  }
+
+    public void SekiChoiceSelection(){
+      TextBox.GetComponent<Text>().text = "Okay do you want my nose, face, left eye, right eye or mouth?";
+      ChoiceMade=2;
+  }
+
+    public void AzureChoiceSelection(){
+      TextBox.GetComponent<Text>().text = "Okay do you want my nose, face, left eye, right eye or mouth?";
+      ChoiceMade=3;
+  }
+
+
+
 
 
     // Update is called once per frame
     void Update()
     {
+        if  (ChoiceMade >=1){
+            ChoiceQing.SetActive (false);
+            ChoiceSeki.SetActive (false);
+            ChoiceAzure.SetActive (false);
+        }
         
     }
 }
