1e9038c87390e9279a0c8f99bbab04a191a600f1
zoezoeokzoe <xzhuoxi@gmail.com>
2022-04-09 21:54:23 +0800

    working  on choice options

---
+++ UXG_ClassB_Assignment3/Assets/Scripts/Own Scripts/ChoiceScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceScript : MonoBehaviour
+{
+  public GameObject TextBox;
+  public GameObject ChoiceQing;
+  public GameObject ChoiceSeki;
+  public GameObject ChoiceAzure;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
